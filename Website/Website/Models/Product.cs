45939693b5ebe22543d4359231917747b92df93a
Keyla C. <Keylachristopher@outlook.com>
2024-11-18 15:58:38 -0500

    Working on Navbar and basic visuals

---
+++ Website/Website/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Website.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public int CategoryId { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public bool IsInStock { get; set; }
+
+    }
+}
